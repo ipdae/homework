7773e46b2d7e831aa7fae59ae517c011d4f687a1
YANG CHUN UNG <qooraven@gmail.com>
2013-01-09 20:40:54 +0900

    Update stackqueue2.py

--- stackqueue2.py
+++ stackqueue2.py
@@ -1,33 +1,18 @@
 import sys
 
-def main():
-	print '1. Stack'
-	print '2. Queue'
-	print '3. Quit'
-	
-	num = int(raw_input('choose number :'))
-
-	if num == 1:
-		stack()
-	elif num == 2:
-		queue()
-	elif num == 3:
-		quit()
-	else:
-		wrong()
 		
 def stack():
 	stack = [] #Stack create
-	a = 0
+	a = 0 
 	while a < 10:
-		a = a + 1
+		a = a + 1	
 		stack.append(a) #Stack PUSH
 	print stack
 	
 	while stack:
 		print stack 
 		stack.pop() #Stack POP
-	main()
+	
 		
 def queue():
 	queue = [] #Queue create
@@ -40,12 +25,10 @@ def queue():
 	while queue:
 		print queue
 		queue.pop(0) #Queue GET
-	main()
 	
-def wrong():
-	print 'sorry wrong number'
-	main()
 	
-def quit():
-	sys.exit()
-main()
+
+	
+stack()
+queue()
+sys.exit()
