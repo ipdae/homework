8ca4a59c1a8b5b0634e1ae269289ccbd946869ba
YANG CHUN UNG <qooraven@gmail.com>
2013-01-09 23:23:50 +0900

    Update stackqueue2.py

--- stackqueue2.py
+++ stackqueue2.py
@@ -4,11 +4,13 @@ import sys
 def stack():
 	stack = [] #Stack create
 	a = 0 
+	print 'Stack PUSH'
 	while a < 10:
 		a = a + 1	
 		stack.append(a) #Stack PUSH
 	print stack
 	
+	print 'Stack POP'
 	while stack:
 		print stack 
 		stack.pop() #Stack POP
@@ -17,11 +19,13 @@ def stack():
 def queue():
 	queue = [] #Queue create
 	a = 0
+	print 'Queue PUT'
 	while a < 10:
 		a = a + 1
-		queue.append(a) #Queue PUSH
+		queue.append(a) #Queue PUT
 	print queue
 	
+	print 'Queue GET'
 	while queue:
 		print queue
 		queue.pop(0) #Queue GET
